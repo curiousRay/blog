8d6461244365cad758cf548ce7b6e1b4a9e39fdb
curiousRay <leilei199708@gmail.com>
2019-09-12 18:34:24 +0800

    test

--- source/conf.py
+++ source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_baseurl = 'https://curiousray.github.io/blog/'
+html_baseurl = ''
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
