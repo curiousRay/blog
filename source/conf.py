c9f800b2fa6534b21940a729344250b68e1e262d
curiousRay <leilei199708@gmail.com>
2019-09-13 15:31:51 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -49,13 +49,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['style.css']
+
+# add custom css files
+def setup(app):
+    app.add_stylesheet('style.css')
+
+html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
     'canonical_url': '',
@@ -74,4 +78,3 @@ mathjax_path = 'https://cdn.bootcss.com/mathjax/2.7.6/MathJax.js?config=TeX-AMS-
 
 # code highlight
 pygments_style = 'trac'
-
